f99c2a603422c9cc7781df561e4ad6a0816d591b
ElitLien <supervlad586@gmail.com>
2020-10-28 15:27:32 +0200

    second master commit

--- PR4.1/PR4.1.cpp
+++ PR4.1/PR4.1.cpp
@@ -7,39 +7,38 @@ int main()
 {
 	int k, N;
 	double S;
-	cout << "k = "; cin >> k;
 	cout << "N = "; cin >> N;
 
 	S = 0;
 	k = 1;
 	while (k <= N)
 	{
-		S += (1 + sqrt(1 + k * k)) / k * k;
+		S += (1 + sqrt(1 + k * k)) / (k * k);
 		k++;
 	}
-	cout << S << "1" << endl;
+	cout << S << "1)" << endl;
 
 	S = 0;
 	k = 1;
 	do {
-		S += (1 + sqrt(1 + k * k)) / k * k;
+		S += (1 + sqrt(1 + k * k)) / (k * k);
 		k++;
 	} while (k <= N);
-	cout << S << "2" << endl;
+	cout << S << "2)" << endl;
 
 	S = 0;
 	for (k = 1; k <= N; k++)
 	{
-		S += (1 + sqrt(1 + k * k)) / k * k;
+		S += (1 + sqrt(1 + k * k)) / (k * k);
 	}
-	cout << S << "3" << endl;
+	cout << S << "3)" << endl;
 
 	S = 0;
 	for (k = N; k >= 1; k--)
 	{
-		S += (1 + sqrt(1 + k * k)) / k * k;
+		S += (1 + sqrt(1 + k * k)) / (k * k);
 	}
-	cout << S << "4" << endl;
+	cout << S << "4)" << endl;
 	
 	return 0;
 }
